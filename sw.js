e9f9d494d97beb34749684b46f31b41f0f5299ad
n588nrsg95-dev <n588nrsg95@privaterelay.appleid.com>
2026-06-29 11:40:48 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='fahrtenbuch-v5-23';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css?v=5.23','./app.js?v=5.23']))) });
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
